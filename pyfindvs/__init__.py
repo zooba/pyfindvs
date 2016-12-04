92e856f8f069b342f321c9a196f41b485074031c
Steve Dower <steve.dower@microsoft.com>
2016-12-03 23:08:02 -0800

    Updates for first release

--- pyfindvs/__init__.py
+++ pyfindvs/__init__.py
@@ -1,3 +1,13 @@
+#-------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation
+# All rights reserved.
+#
+# Distributed under the terms of the MIT License
+#-------------------------------------------------------------------------
+
+__author__ = 'Steve Dower <steve.dower@microsoft.com>'
+__version__ = '0.1.0'
+
 import pyfindvs._helper as _helper
 
 __all__ = ['VisualStudioInstance', 'findall', 'findwithall', 'findwithany']
